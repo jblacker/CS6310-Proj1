5942b330e55bd093c0ae431fa66be839960950c7
Derek Finlinson <dcfinlinson@gmail.com>
2014-09-11 10:03:40 -0600

    Added Twfahp project with Demo class and implemented HeatedPlate class

---
+++ src/edu/gatech/omscs/cs6310/Twfahp/Twafhp.java
@@ -0,0 +1,109 @@
+package edu.gatech.omscs.cs6310.Twfahp;
+
+import edu.gatech.omscs.cs6310.Interfaces.HeatedPlate;
+
+public class Twafhp implements HeatedPlate {
+
+	public Twafhp() {
+		
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.gatech.omscs.cs6310.Interfaces.HeatedPlate#getDimension()
+	 */
+	@Override
+	public int getDimension() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.gatech.omscs.cs6310.Interfaces.HeatedPlate#setDimension(int)
+	 */
+	@Override
+	public void setDimension(int dimension) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.gatech.omscs.cs6310.Interfaces.HeatedPlate#getLeftEdgeTemp()
+	 */
+	@Override
+	public int getLeftEdgeTemp() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.gatech.omscs.cs6310.Interfaces.HeatedPlate#setLeftEdgeTemp(int)
+	 */
+	@Override
+	public void setLeftEdgeTemp(int temp) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.gatech.omscs.cs6310.Interfaces.HeatedPlate#getRightEdgeTemp()
+	 */
+	@Override
+	public int getRightEdgeTemp() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.gatech.omscs.cs6310.Interfaces.HeatedPlate#setRightEdgeTemp(int)
+	 */
+	@Override
+	public void setRightEdgeTemp(int temp) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.gatech.omscs.cs6310.Interfaces.HeatedPlate#getTopEdgeTemp()
+	 */
+	@Override
+	public int getTopEdgeTemp() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.gatech.omscs.cs6310.Interfaces.HeatedPlate#setTopEdgeTemp(int)
+	 */
+	@Override
+	public void setTopEdgeTemp(int temp) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.gatech.omscs.cs6310.Interfaces.HeatedPlate#getBottomEdgeTemp()
+	 */
+	@Override
+	public int getBottomEdgeTemp() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.gatech.omscs.cs6310.Interfaces.HeatedPlate#setBottomEdgeTemp(int)
+	 */
+	@Override
+	public void setBottomEdgeTemp(int temp) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.gatech.omscs.cs6310.Interfaces.HeatedPlate#calculateLatticePoints()
+	 */
+	@Override
+	public Float[][] calculateLatticePoints() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
