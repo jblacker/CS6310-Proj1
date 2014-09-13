f5952bade0be5d9fc34031d2fe2082d46735fb58
Jordan Blacker <jordan.blacker@gatech.edu>
2014-09-13 01:12:01 -0400

    Updated code to read arguments in Tpfahp.Demo

--- src/edu/gatech/omscs/cs6310/Tpfahp/Demo.java
+++ src/edu/gatech/omscs/cs6310/Tpfahp/Demo.java
@@ -1,46 +1,87 @@
 package edu.gatech.omscs.cs6310.Tpfahp;
 
-import java.util.regex.*;
-
 public class Demo {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		if(args.length < 10 || args.length > 10)
+		if(args.length != 10)
 		{
 			System.out.println("Invalid number of arguments");
 			return;
 		}
 		
 		TpfahpDiffusion heatedPlate = new TpfahpDiffusion();
-		try {
-			for(int i = 0; i < args.length; i += 2) {
-				if(args[i] == "-d" || args[i] == "-D") {
-					int dim = Integer.parseInt(args[i+1]);
-					if(dim >= 1) {
-						heatedPlate.setDimension(dim);
+		for(int i = 0; i < args.length; i += 2) {
+			int temp;
+			try {
+				temp = Integer.parseInt(args[i+1]);
+			}
+			catch(NumberFormatException ex) {
+				System.out.println("Expected an integer every other argument, invalid input.");
+				return;
+			}	
+			
+			switch(args[i]) {
+			case "-d":
+			case "-D":
+				if(temp >= 1) {
+					heatedPlate.setDimension(temp);
+					break;
+				}
+				else {
+					System.out.println("Dimension argument must be greater than or equal to 1");
+					System.exit(1);
+					return;
+				}
+				case "-l":
+				case "-L":
+					if(temp > 100 || temp < 0) {
+						heatedPlate.setLeftEdgeTemp(temp);
+						break;
 					}
 					else {
-						System.out.println("Dimensions must be greater or equal to 1");
+						System.out.println("Temperature agruments must be between 0 - 100");
+						System.exit(1);
+						return;
 					}
-				}
-				else if(args[i] == "-l" || args[i] == "-L") {
-					int left = Integer.parseInt(args[i+1]);
-					if(left >= 0 || left <= 100) {
-						heatedPlate.setLeftEdgeTemp(left);
+				case "-r":
+				case "-R":
+					if(temp > 100 || temp < 0) {
+						heatedPlate.setRightEdgeTemp(temp);
+						break;
 					}
 					else {
-						//TODO: Finish or change to SWITCH if we upgrade to JDK 7
+						System.out.println("Temperature agruments must be between 0 - 100");
+						System.exit(1);
+						return;
 					}
-				}
+				case "-b":
+				case "-B":
+					if(temp > 100 || temp < 0) {
+						heatedPlate.setBottomEdgeTemp(temp);
+						break;
+					}
+					else {
+						System.out.println("Temperature agruments must be between 0 - 100");
+						System.exit(1);
+						return;
+					}
+				case "-t":
+				case "-T":
+					if(temp > 100 || temp < 0) {
+						heatedPlate.setTopEdgeTemp(temp);
+						break;
+					}
+					else {
+						System.out.println("Temperature agruments must be between 0 - 100");
+						System.exit(1);
+						return;
+					}	
 			}
 		}
-		catch(NumberFormatException ex) {
-			System.out.println("Expecting an integer, invalid argument passed.");
-			return;
-		}
+		
+		//TODO: Calculate & Display
 	}
-
 }
