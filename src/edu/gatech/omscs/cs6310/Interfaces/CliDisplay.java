562dc7e69fbb68cb993e6a11784cc37ea0136b2a
Jordan Blacker <jordan.blacker@gatech.edu>
2014-09-15 22:41:44 -0400

    CliDisplay now has basic error handling

--- src/edu/gatech/omscs/cs6310/Interfaces/CliDisplay.java
+++ src/edu/gatech/omscs/cs6310/Interfaces/CliDisplay.java
@@ -9,13 +9,18 @@ import java.util.List;
 public class CliDisplay {
 	
 	public static void displayHeatedPlate(HeatedPlate plate) {
-		List<List<Number>> results = plate.calculateLatticePoints();
-		System.out.println("");
-		for(int x = 1; x < results.size()-2; x++) {
-			for(int y = 1; y < results.get(x).size()-2; y++) {
-				System.out.format("  %.2f  ", results.get(x).get(y).floatValue());
-			}
+		try {
+			List<List<Number>> results = plate.calculateLatticePoints();
 			System.out.println("");
+			for(int x = 1; x < results.size()-2; x++) {
+				for(int y = 1; y < results.get(x).size()-2; y++) {
+					System.out.format("  %.2f  ", results.get(x).get(y).floatValue());
+				}
+				System.out.println("");
+			}
+		}
+		catch(PlateNotInitializedException ex) {
+			System.out.println("Heated Plate not initialized properly.");
 		}
 	}
 	
