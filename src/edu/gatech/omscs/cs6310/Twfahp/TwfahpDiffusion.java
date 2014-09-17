0de7211b64ee591e0f071e89c9d3b8e2d7a1ec59
Derek Finlinson <dcfinlinson@gmail.com>
2014-09-17 15:55:00 -0600

    Added comments

--- src/edu/gatech/omscs/cs6310/Twfahp/TwfahpDiffusion.java
+++ src/edu/gatech/omscs/cs6310/Twfahp/TwfahpDiffusion.java
@@ -7,87 +7,157 @@ import java.util.List;
 import edu.gatech.omscs.cs6310.Interfaces.HeatedPlate;
 import edu.gatech.omscs.cs6310.Interfaces.PlateNotInitializedException;
 
+/**
+ * Calculate diffusion of temperature on a plate using Floats 
+ */
 public class TwfahpDiffusion implements HeatedPlate {
 
+	/* Maximum number of iterations that can be performed. Used to avoid an infinite loop */
 	private final int MAXIMUM_ITERATIONS = 150;
+	
+	/* Difference in temperature signifying the plate temperature has stabilized */
 	private final Float MINIMUM_DIFFERENCE = 0.001F;
 	
+	/* Number of dimensions to split the plate into */
 	private int dimension;
+	
+	/* Intial temperature of the left edge of the plate */
 	private int initLeftEdgeTemp;
+	
+	/* Intial temperature of the right edge of the plate */
 	private int initRightEdgeTemp;
+	
+	/* Intial temperature of the top edge of the plate */
 	private int initTopEdgeTemp;
+	
+	/* Intial temperature of the bottom edge of the plate */
 	private int initBottomEdgeTemp;
 	
+	/* Length of time required for plate temperature to stabilize */
 	private long lastRunTime;
+	
+	/* Number of iterations required for plate temperature to stabilize */
 	private int lastIterationCount;
 	
+	/* Final temperature of the different points on the plate*/
 	private Float[][] latticePoints;
 	
+	/**
+	 * Default constructor
+	 * Sets initial dimension and temperature values to 0
+	 */
 	public TwfahpDiffusion() {
 		this.dimension = 0;
 		this.initLeftEdgeTemp = 0;
 		this.initRightEdgeTemp = 0;
 		this.initTopEdgeTemp = 0;
 		this.initBottomEdgeTemp = 0;
-	}	
+	}
 	
+	/**
+	 * Get dimension
+	 */
 	@Override
 	public int getDimension() {
 		return this.dimension;
 	}
 
+	/**
+	 * Set dimension
+	 * @param dimension
+	 * Dimension of the plate
+	 */
 	@Override
 	public void setDimension(int dimension) {
 		this.dimension = dimension;
 	}
 
+	/**
+	 * Get left edge temperature
+	 */
 	@Override
 	public int getLeftEdgeTemp() {
 		return this.initLeftEdgeTemp;
 	}
 
+	/**
+	 * Set left edge temperature
+	 * @param temp
+	 * Temperature of the left edge
+	 */
 	@Override
 	public void setLeftEdgeTemp(int temp) {
 		this.initLeftEdgeTemp = temp;
 	}
 
+	/**
+	 * Get right edge temperature
+	 */
 	@Override
 	public int getRightEdgeTemp() {
 		return this.initRightEdgeTemp;
 	}
 
+	/**
+	 * Set right edge temperature
+	 * @param temp
+	 * Temperature of the right edge
+	 */
 	@Override
 	public void setRightEdgeTemp(int temp) {
 		this.initRightEdgeTemp = temp;
 	}
 
+	/**
+	 * Get top edge temperature
+	 */
 	@Override
 	public int getTopEdgeTemp() {
 		return this.initTopEdgeTemp;
 	}
 
+	/**
+	 * Set top edge temperature
+	 * @param temp
+	 * Temperature of the top edge
+	 */
 	@Override
 	public void setTopEdgeTemp(int temp) {
 		this.initTopEdgeTemp = temp;
 	}
 
+	/**
+	 * Get bottom edge temperature
+	 */
 	@Override
 	public int getBottomEdgeTemp() {
 		return this.initBottomEdgeTemp;
 	}
 
+	/**
+	 * Set bottom edge temperature
+	 * @param temp
+	 * Temperature of the bottom edge
+	 */
 	@Override
 	public void setBottomEdgeTemp(int temp) {
 		this.initBottomEdgeTemp = temp;
 	}
 
+	/**
+	 * Calculate plate temperature diffusion
+	 * @return 
+	 * Returns a List of Lists containing the different points of the plate
+	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<List<Float>> calculateLatticePoints() throws PlateNotInitializedException {
-		
+		// Calculate the diffusion for the heated plate
 		this.calculateDiffusion();
+		
 		List<List<Float>> points = new ArrayList<List<Float>>();
 		
+		// Convert latticePoints array to a List of Lists
 		for (Float[] axis : this.latticePoints) {
 				points.add(Arrays.asList(axis));
 			}
@@ -95,16 +165,26 @@ public class TwfahpDiffusion implements HeatedPlate {
 		return points;
 	}
 
+	/**
+	 * Get time spent running calculation
+	 */
 	@Override
 	public long getCalculationTime() {
 		return this.lastRunTime;
 	}
 	
+	/**
+	 * Get total iterations used running calculation
+	 */
 	@Override
 	public int getIterationsUsed() {
 		return this.lastIterationCount;
 	}
 	
+	/**
+	 * Calculate the time and iterations required to diffuse heat through a plate
+	 * @throws PlateNotInitializedException
+	 */
 	public void calculateDiffusion() throws PlateNotInitializedException {
 		if(this.dimension == 0) {
 			throw new PlateNotInitializedException("Dimensions must be set");
@@ -112,6 +192,7 @@ public class TwfahpDiffusion implements HeatedPlate {
 		
 		long startTime = System.nanoTime();
 		
+		// Initialize the plates
 		Float[][] newPlate = initializePlate();
 		Float[][] oldPlate = initializePlate();
 		
@@ -128,17 +209,25 @@ public class TwfahpDiffusion implements HeatedPlate {
 		        }
 			}
 
+			// Calculate temperature difference between original and updated plate
 			difference = getTempDifference(newPlate, oldPlate);
 			iterations++;
 			
+			// Update original plate with updated plate temperatures
 			oldPlate = deepCopySwap(newPlate);
 		} while(difference >= MINIMUM_DIFFERENCE && iterations < MAXIMUM_ITERATIONS);
 		
+		// Set final values
 		this.latticePoints = newPlate;
 		this.lastRunTime = System.nanoTime() - startTime;
 		this.lastIterationCount = iterations;
 	}
 	
+	/**
+	 * Initialize the plate based on dimension and edge temperatures
+	 * @return
+	 * Returns a plate as an Array of Floats
+	 */
 	private Float[][] initializePlate() {
 		Float[][] plate = new Float[this.dimension + 2][this.dimension + 2];
 		
@@ -164,6 +253,13 @@ public class TwfahpDiffusion implements HeatedPlate {
 		return plate;
 	}
 		
+	/**
+	 * Copy the updated plate to the original plate
+	 * @param original
+	 * The original plate
+	 * @return
+	 * Returns a copy of the original plate
+	 */
 	private Float[][] deepCopySwap(Float[][] original) {
 		if (original == null) {
 			return null;
@@ -177,9 +273,19 @@ public class TwfahpDiffusion implements HeatedPlate {
 		return result;
 	}
 
+	/**
+	 * Get the temperature difference between the original and updated plates
+	 * @param newPlate
+	 * The updated plate
+	 * @param oldPlate
+	 * The original plate
+	 * @return
+	 * Returns the difference in total temperature as a Float
+	 */
 	private Float getTempDifference(Float[][] newPlate, Float[][] oldPlate) {
 		Float totalNewTemp = 0F, totalOldTemp = 0F;
 		
+		// Calculate total temperature of the plates
 		for (int i = 1; i <= this.dimension; i++) {
 			for (int j = 1; j <= this.dimension; j++) {
 				totalNewTemp += newPlate[i][j];
